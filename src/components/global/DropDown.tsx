e1edf2f0e34530e26caf54e815961b42298c8c93
rahulstratagile <rahul.v@stratagile.com>
2024-11-11 11:08:16 +0530

    the functionality of the "Other" option and custom input field.

--- src/components/global/DropDown.tsx
+++ src/components/global/DropDown.tsx
@@ -2,24 +2,24 @@
 
 import React, { useEffect, useRef, useState } from 'react'
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md'
+import TextField from './TextField';
 
 /**
  * DropDown Component
  *
- * A custom dropdown component built using React. 
- * It allows users to select an option from a provided list.
- * The dropdown closes when clicking outside its container.
+ * A custom dropdown component that allows users to select an option from a provided list. 
+ * The dropdown also includes an option to select "Other" and specify a custom input.
+ * It closes automatically when clicking outside the dropdown container.
  *
  * @component
- * @param {object} props - The component props.
+ * @param {DropDownProps} props - The component props.
  * @param {string} props.selectPlaceHolder - Placeholder text displayed when no option is selected.
- * @param {string} [props.customClass] - Optional custom CSS classes for styling the dropdown.
- * @param {string} [props.dropdownWidthClass] - Optional CSS classes for controlling dropdown width.
+ * @param {string} [props.customClass] - Optional custom CSS classes for styling the dropdown container.
+ * @param {string} [props.dropdownWidthClass] - Optional CSS classes for controlling the dropdown width.
  * @param {Array.<{ label: string, value: string }>} props.optionLists - Array of options for the dropdown.
- * Each option includes a label (display text) and a value.
+ *  Each option includes a label (display text) and a value to be selected.
  *
- *
- * @returns {JSX.Element} A dropdown component with selectable options.
+ * @returns {JSX.Element} A dropdown component with selectable options and an "Other" input field.
  */
 
 
@@ -36,8 +36,9 @@ interface DropDownProps {
 }
 
 const DropDown: React.FC<DropDownProps> = ({ optionLists, selectPlaceHolder, customClass = "", dropdownWidthClass = "" }) => {
-    const [showOptionList, setShowOptionList] = useState(false)
     const [selectedOption, setSelectedOption] = useState('')
+    const [showOptionList, setShowOptionList] = useState(false)
+    const [isOtherSelected , setIsOtherSelected] = useState(false)
 
     const dropdownRef = useRef<HTMLDivElement | null>(null)
 
@@ -48,6 +49,12 @@ const DropDown: React.FC<DropDownProps> = ({ optionLists, selectPlaceHolder, cus
     const handleSelectList = (value: string) => {
         setSelectedOption(value)
         setShowOptionList(false)
+        setIsOtherSelected(false)
+    }
+
+    const hanleOtherSelectedOption = (value : string) => {
+        setIsOtherSelected(true)
+        setSelectedOption(value)
     }
 
     useEffect(() => {
@@ -63,24 +70,30 @@ const DropDown: React.FC<DropDownProps> = ({ optionLists, selectPlaceHolder, cus
     }, [])
 
     return (
-        <div ref={dropdownRef} className={`flex flex-col gap-2 relative w-[260px] ${dropdownWidthClass}`}>
-            <div onClick={handleDropDownList} className={`h-16 shadow-md rounded-lg flex items-center justify-between px-4 cursor-pointer ${customClass}`}>
+        <div ref={dropdownRef} className={`flex flex-col gap-[7px] w-[260px] ${dropdownWidthClass}`}>
+            <div onClick={handleDropDownList} className={`h-[60px] shadow-dropdown-shadow rounded-lg flex items-center justify-between px-4 py-5 cursor-pointer ${customClass}`}>
                 <p className={`text-base ${!selectedOption ? "text-[#666666]" : ""}`}>{selectedOption || selectPlaceHolder}</p>
                 <span className={`cursor-pointer transition-transform ${showOptionList ? "rotate-180" : "" }`}><MdOutlineKeyboardArrowDown size={25} /></span>
             </div>
-            <div className={`w-[260px] z-50 h-auto absolute bg-white top-20 flex flex-col shadow-md rounded-lg px-4 transition-all duration-300 ease-in-out overflow-hidden ${showOptionList ? 'max-h-60 opacity-100' : 'max-h-0 opacity-0'} ${dropdownWidthClass}`}>
+            <div className={`w-[260px] z-50 h-auto bg-white flex flex-col shadow-dropdown-shadow rounded-lg transition-all duration-300 ease-in-out overflow-hidden ${showOptionList ? 'max-h-60 opacity-100' : 'max-h-0 opacity-0'} ${dropdownWidthClass}`}>
                 {showOptionList && 
-                    optionLists.map((options, index) => (
+                    <>
+                    {optionLists.map((options, index) => (
                         <div 
                             key={index} 
                             onClick={() => handleSelectList(options.value)} 
-                            className='h-11 flex items-center text-base cursor-pointer'
+                            className='h-11 px-4 py-3 flex items-center text-base cursor-pointer'
                         >
                             {options.label}
                         </div>
-                    ))
+                    ))}
+                        <div onClick={() => hanleOtherSelectedOption("Other")} className='h-11 px-4 py-3 flex items-center text-base cursor-pointer'>Other</div>
+                    </>
                 }
             </div>
+            {isOtherSelected &&
+                <TextField customClass='h-11' placeholder='Specify other target audiences' />
+            }
         </div>
     )
 }
