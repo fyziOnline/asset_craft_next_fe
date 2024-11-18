fcc6de97b76bc006317eec29072504d322f4d463
rahulstratagile <rahul.v@stratagile.com>
2024-11-18 18:00:28 +0530

    created profile page and add state management for input fields

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,11 @@
 import { UserIcon } from "@/assets/icons/AppIcons"
+import Link from 'next/link'
 
 const Header:React.FC = () => {
   return (
-    <header className="fixed top-0 right-0 left-[5.4rem] bg-off-white-primary p-[0.85rem]">
+    <header className="fixed top-0 right-0 left-[5.4rem] bg-off-white-primary p-[0.85rem] z-50">
         <div className="w-full flex justify-end">
-            <UserIcon />
+          <Link href="/Profile" className="cursor-pointer"><UserIcon /></Link>
         </div>
     </header>
   )
