4c931e0df2f6d4ecee985eadce1f2d7b139cc733
Muhammad Fyrooz Razi C <151521733+FyroozStrat@users.noreply.github.com>
2024-11-20 16:15:57 +0530

    template generation screen [select template], dynamic routes and components

--- src/app/my-projects/[project_name]/[campaign_name]/linkedin/page.tsx
+++ src/app/my-projects/[project_name]/[campaign_name]/linkedin/page.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb from "@/components/global/Breadcrumb"
+import AssetGenerationHeader from "@/components/layout/AssetGenerationHeader"
 import TemplateSelectionContainer from "@/components/layout/TemplateSelectionContainer"
 import LayoutWrapper from "@/layout/LayoutWrapper"
 import { FC } from "react"
@@ -7,8 +8,6 @@ interface ProjectAssetProp {
   params: {
     project_name: string
     campaign_name : string
-    asset_name: string
-
   }
 }
 interface Template {
@@ -17,20 +16,20 @@ interface Template {
   template_name: string
 }
 
-const dummyTemplateData:Template[] = [
+const dummyLinkedinTemplateData:Template[] = [
   {
-    imageUrl : '/images/email_templates/template_1.png',
-    template_name :'Event Invite',
+    imageUrl : '/images/linkedin_templates/template_1.png',
+    template_name :'Text Post',
     id : '1'
   },
   {
-    imageUrl : '/images/email_templates/template_2.png',
-    template_name :'Webinar',
+    imageUrl : '/images/linkedin_templates/template_2.png',
+    template_name :'Media Post',
     id : '2'
   },
   {
-    imageUrl : '/images/email_templates/template_3.png',
-    template_name :'Product Features',
+    imageUrl : '/images/linkedin_templates/template_3.png',
+    template_name :'Multiple Media Post',
     id:'3'
   }
 ]
@@ -39,15 +38,13 @@ const ProjectAssetPage: FC<ProjectAssetProp> = async ({ params }) => {
   const takeData = async () => {
     return params
   }
-  const {project_name,campaign_name,asset_name} = await takeData()
+  const {project_name,campaign_name} = await takeData()
 
   return (
     <LayoutWrapper layout="main">
-      <div className="border-grey-200 border-b-[1px] border-solid py-[2rem] px-[1.5rem] mb-4">
-        <Breadcrumb projectName={project_name} TaskName={campaign_name} TaskType={asset_name}/>
-      </div>
+      <AssetGenerationHeader params={{project_name,campaign_name}}/>
       <div className=" px-[1.5rem]">
-        <TemplateSelectionContainer templateData={dummyTemplateData} title="Select one of the templates"/>
+        <TemplateSelectionContainer aspect_ratio="2 / 3" templateData={dummyLinkedinTemplateData} title="Select one of the templates"/>
       </div>
     </LayoutWrapper>
   )
