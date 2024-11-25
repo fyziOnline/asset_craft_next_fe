83ae140b0fb98a7aeb09ed911a907d2970052680
rahulstratagile <rahul.v@stratagile.com>
2024-11-25 10:57:58 +0530

    landing page creation

--- src/layout/LayoutWrapper.tsx
+++ src/layout/LayoutWrapper.tsx
@@ -2,21 +2,22 @@ import { FC, ReactNode } from "react"
 import MainLayout from "./MainLayout"
 import HomeLayout from "./HomeLayout"
 
-type LayoutType = 'main' | 'home'
+type LayoutType = "main" | "home";
 
 interface WrapperProps {
-    children : ReactNode
-    layout ?: LayoutType
+    children: ReactNode;
+    layout?: LayoutType;
 }
 
 const Layout = {
-    main : MainLayout,
-    home : HomeLayout
-}
+    main: MainLayout,
+    home: HomeLayout,
+};
 
-const LayoutWrapper:FC<WrapperProps> = ({children,layout='main'}) => {
- const WrappingLayout = Layout[layout]
-  return <WrappingLayout>{children}</WrappingLayout>
+const LayoutWrapper:FC<WrapperProps> = ({ children, layout='main' }) => {
+    const WrappingLayout = Layout[layout];
+
+    return <WrappingLayout>{children}</WrappingLayout>
 }
 
 export default LayoutWrapper
