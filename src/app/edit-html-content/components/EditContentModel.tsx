e233f8f45cd287148c735b5d44f6f7ff3289d390
Khang Lam <lamchikhang.it@gmail.com>
2024-12-10 15:04:06 +0700

    custom theme edit

--- src/app/edit-html-content/components/EditContentModel.tsx
+++ src/app/edit-html-content/components/EditContentModel.tsx
@@ -8,6 +8,57 @@ import Button from '@/components/global/Button';
 import { ApiService } from '@/lib/axios_generic';
 import { urls } from '@/apis/urls';
 
+import { createTheme, ThemeProvider } from '@mui/material';
+
+
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#01A982', // Primary color
+        },
+        secondary: {
+            main: '#D9D9D9', // Secondary color
+        },
+        background: {
+            default: '#f7f7f7', // Background color
+        },
+    },
+    typography: {
+        fontFamily: 'Roboto, Arial, sans-serif',
+        body1: {
+            fontSize: '0.9rem',
+        },
+    },
+    components: {
+        MuiInputBase: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px',
+                    backgroundColor: '#fff',
+                    padding: '3px',
+                },
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    textTransform: 'none',
+                    fontSize: '1rem',
+                    fontWeight: '500',
+                },
+            },
+        },
+        MuiGrid: {
+            styleOverrides: {
+                root: {
+                    gap: '14px',
+                    marginTop: "6px"
+                },
+            },
+        },
+    },
+});
+
 const EditContentModel = ({ setIsShowModelEdit }: any) => {
     const { contextData, setContextData } = useAppData();
     const [assetHTML, setAssetHTML] = useState(contextData.AssetHtml)
@@ -105,49 +156,51 @@ const EditContentModel = ({ setIsShowModelEdit }: any) => {
     };
 
     return (
-        <div onClick={handleClick} className="fixed z-[1000] left-0 right-0 top-0 bottom-0 bg-black bg-opacity-55 flex items-center justify-center">
-            <div className='w-[90vw] h-[94vh] bg-white rounded-md relative flex flex-col'>
-                <div className='border-b border-solid border-[#D9D9D9] px-4 pb-4 flex flex-wrap'>
-                    {assetHTML?.assetBlocks?.map((item, index) => {
-                        return (
-                            <div onClick={() => { onSelectBlock(item, index) }}
-                                className={`p-2 mr-2 mt-4 rounded-md cursor-pointer ${assetBlockSelected.assetBlockDataVersionID === item.assetBlockDataVersionID ? `text-white bg-[#01A982]` : `text-black bg-[#e4e4e4]`}`}
-                                key={index}>{item.name?.replaceAll("_", " ")}</div>
-                        )
-                    })}
-                </div>
+        <ThemeProvider theme={customTheme}>
+            <div onClick={handleClick} className="fixed z-[1000] left-0 right-0 top-0 bottom-0 bg-black bg-opacity-55 flex items-center justify-center">
+                <div className='w-[90vw] h-[94vh] bg-white rounded-md relative flex flex-col'>
+                    <div className='border-b border-solid border-[#D9D9D9] px-4 pb-4 flex flex-wrap'>
+                        {assetHTML?.assetBlocks?.map((item, index) => {
+                            return (
+                                <div onClick={() => { onSelectBlock(item, index) }}
+                                    className={`p-2 mr-2 mt-4 rounded-md cursor-pointer ${assetBlockSelected.assetBlockDataVersionID === item.assetBlockDataVersionID ? `text-white bg-[#01A982]` : `text-black bg-[#e4e4e4]`}`}
+                                    key={index}>{item.name?.replaceAll("_", " ")}</div>
+                            )
+                        })}
+                    </div>
 
-                <div className='flex-1 overflow-y-scroll scrollbar-hide px-5 py-2'>
-                    <JsonForms
-                        schema={schema}
-                        data={blockData}
-                        renderers={materialRenderers}
-                        cells={materialCells}
-                        // uischema={uiSChema}
-                        onChange={onHandleEditData}
-                    />
-                </div>
-                <div className='border-t border-solid border-[#D9D9D9] p-4 flex justify-end'>
-                    <Button
-                        handleClick={() => { setIsShowModelEdit(false) }}
-                        buttonText='Close'
-                        showIcon={false}
-                        textStyle='text-[1rem] font-base'
-                        textColor="text-[#00A881]"
-                        backgroundColor="bg-[#fff]"
-                        customClass='static ml-[0px] px-[35px] py-[10px] group-hover:border-white mr-[20px] border border-solid border-[#00A881]' />
-                    <Button
-                        handleClick={onSaveAllAndClose}
-                        disabled={isLoading}
-                        buttonText={isLoading ? 'Saving...' : 'Save All'}
-                        showIcon={false}
-                        textStyle='text-[1rem] font-base text-[#00A881]'
-                        textColor="text-[#fff]"
-                        backgroundColor={isLoading ? "bg-[#00A881]" : "bg-custom-gradient-green"}
-                        customClass='static ml-[0px] px-[35px] py-[10px] group-hover:border-white' />
+                    <div className='flex-1 overflow-y-scroll scrollbar-hide px-5 py-2'>
+                        <JsonForms
+                            schema={schema}
+                            data={blockData}
+                            renderers={materialRenderers}
+                            cells={materialCells}
+                            // uischema={uiSChema}
+                            onChange={onHandleEditData}
+                        />
+                    </div>
+                    <div className='border-t border-solid border-[#D9D9D9] p-4 flex justify-end'>
+                        <Button
+                            handleClick={() => { setIsShowModelEdit(false) }}
+                            buttonText='Close'
+                            showIcon={false}
+                            textStyle='text-[1rem] font-base'
+                            textColor="text-[#00A881]"
+                            backgroundColor="bg-[#fff]"
+                            customClass='static ml-[0px] px-[35px] py-[10px] group-hover:border-white mr-[20px] border border-solid border-[#00A881]' />
+                        <Button
+                            handleClick={onSaveAllAndClose}
+                            disabled={isLoading}
+                            buttonText={isLoading ? 'Saving...' : 'Save All'}
+                            showIcon={false}
+                            textStyle='text-[1rem] font-base text-[#00A881]'
+                            textColor="text-[#fff]"
+                            backgroundColor={isLoading ? "bg-[#00A881]" : "bg-custom-gradient-green"}
+                            customClass='static ml-[0px] px-[35px] py-[10px] group-hover:border-white' />
+                    </div>
                 </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
 };
 
