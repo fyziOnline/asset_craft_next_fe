641dab07a1c974856258ea3c02d4f6eaefc1e403
Khang Lam <lamchikhang.it@gmail.com>
2024-12-11 16:00:40 +0700

    convert file to base64

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,22 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const convertFileToBase64 = (file: File): Promise<{ isSuccess: boolean, base64String?: string, error?: any }> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      const base64String = reader.result as string;
+      resolve({ isSuccess: true, base64String });
+    };
+
+    reader.onerror = (error) => {
+      console.error("File conversion error: ", error);
+      reject({ isSuccess: false, error });
+    };
+
+    reader.readAsDataURL(file);
+  });
+};
+
