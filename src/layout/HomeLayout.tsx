12d880110872401aa23f1b06c3bd1c8767902e78
Khang Lam <lamchikhang.it@gmail.com>
2024-12-18 14:39:01 +0700

    fix bug and build

--- src/layout/HomeLayout.tsx
+++ src/layout/HomeLayout.tsx
@@ -1,19 +1,19 @@
-import Footer from '@/components/Layout/Footer'
-import HomeHeader from '@/components/Layout/HomeHeader'
+import Footer from '@/components/layout/Footer'
+import HomeHeader from '@/components/layout/HomeHeader'
 import { FC, ReactNode } from 'react'
 
 interface LayoutProps {
-    children : ReactNode
+  children: ReactNode
 }
 
-const HomeLayout:FC<LayoutProps> = ({children}) => {
+const HomeLayout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className='h-screen w-screen bg-black overflow-hidden'>
-        <HomeHeader />
-        <main  className="px-32 relative overflow-hidden h-full">
-            {children}
-        </main>
-        <Footer />
+      <HomeHeader />
+      <main className="px-32 relative overflow-hidden h-full">
+        {children}
+      </main>
+      <Footer />
     </div>
   )
 }
