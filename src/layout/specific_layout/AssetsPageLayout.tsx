e7111c0b024095625e52e751ba60990d2b9dae9a
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2024-12-30 14:16:06 +0530

    api integration added to campagin and assets pages

--- src/layout/specific_layout/AssetsPageLayout.tsx
+++ src/layout/specific_layout/AssetsPageLayout.tsx
@@ -27,24 +27,33 @@ const AssetsPageLayout: FC<AssetsPageProps> = ({ campaign_data, tableHeadings, h
     <>
       <div className="flex items-center justify-between pt-[1rem] px-[1.5rem]">
         <Breadcrumb TaskType={page} />
-        <span className="pr-10 cursor-pointer" onClick={toggleListType}>{!isList ? <ListIcon /> : <GridIcon />}</span>
+        <span className="pr-10 cursor-pointer" onClick={toggleListType}>
+          {!isList ? <ListIcon /> : <GridIcon />}
+        </span>
       </div>
       <div className="px-4 lg:px-16 xl:px-20">
-        {!isList ?
-          <div className=" asset-grid-layout mt-4  justify-center overflow-auto">
+        {!isList ? (
+          <div className="asset-grid-layout mt-4 justify-center overflow-auto">
             {campaign_data.map((data, index) => (
-              <div key={index}
-              >
+              <div key={index}>
                 <AssetCard data={data} />
               </div>
             ))}
           </div>
-          :
-          <Table listItems={campaign_data} tableHeadings={tableHeadings} arrowInHeadings={headersHavingToggle}
-          />}
+        ) : (
+          campaign_data.length > 0 ? (
+            <Table
+              listItems={campaign_data}
+              tableHeadings={tableHeadings}
+              arrowInHeadings={headersHavingToggle}
+            />
+          ) : (
+            <div className="w-full h-[70vh] flex justify-center items-center text-gray-500">No data available</div>
+          )
+        )}
       </div>
     </>
-  )
+  );
 }
 
 export default AssetsPageLayout
--- src/layout/specific_layout/ProjectPageLayout.tsx
+++ src/layout/specific_layout/ProjectPageLayout.tsx
@@ -7,43 +7,45 @@ import MyProjectCard from '../../components/wrapper/MyProjectCard'
 import Table from '../../components/global/Table'
 
 interface Project {
-  [key:string] : string
+  [key: string]: string
 }
 
 interface ProjectSectionProps {
-    project_data : Project[]
-    onSelectingProjects : (project_name:string)=>void
-    tableHeadings : string[]
-    headersHavingToggle : string[]
-    page:string
-    viewType :'project' | 'campaign'
+  project_data: Project[]
+  onSelectingProjects: (project_name: string) => void
+  tableHeadings: string[]
+  headersHavingToggle: string[]
+  page: string
+  viewType: 'project' | 'campaign'
 }
 
-const ProjectPageLayout:FC<ProjectSectionProps> = ({project_data,onSelectingProjects,tableHeadings,headersHavingToggle,page,viewType}) => {
-  const [isList,setIsList] = useState<Boolean>(true)
+const ProjectPageLayout: FC<ProjectSectionProps> = ({ project_data, onSelectingProjects, tableHeadings, headersHavingToggle, page, viewType }) => {
+  const [isList, setIsList] = useState<Boolean>(true)
   const toggleListType = () => {
-    setIsList(pre=>!pre)
+    setIsList(pre => !pre)
   }
 
   return (
     <>
       <div className="flex items-center justify-between pt-[1rem] px-[1.5rem]">
-          <Breadcrumb TaskType={page}/>
-          <span className="pr-10 cursor-pointer" onClick={toggleListType}>{!isList ? <ListIcon /> : <GridIcon /> }</span>
+        <Breadcrumb TaskType={page} />
+        <span className="pr-10 cursor-pointer" onClick={toggleListType}>{!isList ? <ListIcon /> : <GridIcon />}</span>
       </div>
       <div className="asset-grid-padding">
-          {
-          !isList ? 
+        {
+          !isList ?
             <div className="asset-grid-layout mt-4  justify-center overflow-auto">
-            {project_data.map((data,index)=>(
-              <div key={index}> 
-                <MyProjectCard viewType={viewType} data={data} handleSelectProject={onSelectingProjects} />
-              </div>
-            ))}
+              {project_data.map((data, index) => (
+                <div key={index}>
+                  <MyProjectCard viewType={viewType} data={data} handleSelectProject={onSelectingProjects} />
+                </div>
+              ))}
             </div>
-            :
-          // <Table columnWidths={['8fr', '2fr', '1fr']} listItems={project_data} tableHeadings={tableHeadings} arrowInHeadings={headersHavingToggle} />
-          <Table columnWidths={['8fr', '2fr', '1fr']}  listItems={project_data.map(({ campaignID, ...rest }) => rest)} tableHeadings={tableHeadings} arrowInHeadings={headersHavingToggle} />
+            : (
+              project_data.length > 0 ?
+              <Table viewType={viewType} onSelectingProject={onSelectingProjects} columnWidths={['8fr', '2fr', '1fr']}  listItems={project_data} tableHeadings={tableHeadings} arrowInHeadings={headersHavingToggle} />
+              : <div className="w-full h-[70vh] flex justify-center items-center text-gray-500">No data available</div>
+            )
         }
       </div>
 
