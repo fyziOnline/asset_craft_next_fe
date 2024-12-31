09766b835b7d2b9ae8925d8b4439da5f9d42c928
Khang Lam <lamchikhang.it@gmail.com>
2024-12-31 14:53:31 +0700

    get templates

--- src/hooks/useGetTemplates.ts
+++ src/hooks/useGetTemplates.ts
@@ -17,68 +17,7 @@ export const useGetTemplates = ({ type_page }: GetTemplatesProps) => {
     const queryParams = useSearchParams()
 
     useEffect(() => {
-        if (type_page === ListTypePage.Email) {
-            getTemplates()
-        } else {
-            if (type_page === ListTypePage.LandingPage) {
-                setListTemplates([{
-                    templateImageURL: '/images/landing_templates/landing1.png',
-                    templateName: 'Option 1',
-                    templateID: '1',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/landing_templates/landing2.png',
-                    templateName: 'Option 2',
-                    templateID: '2',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/landing_templates/landing3.png',
-                    templateName: 'Option 3',
-                    templateID: '3',
-                    description: "description"
-                }])
-            } else if (type_page === ListTypePage.CallScript) {
-                setListTemplates([{
-                    templateImageURL: '/images/event_invite_call_script.png',
-                    templateName: 'Event Invite Call Script',
-                    templateID: '1',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/sales_call_script.png',
-                    templateName: 'Sales Call Script',
-                    templateID: '2',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/demo_call_script.png',
-                    templateName: 'Demo Call Script',
-                    templateID: '3',
-                    description: "description"
-                }])
-            } else if (type_page === ListTypePage.LinkedIn) {
-                setListTemplates([{
-                    templateImageURL: '/images/linkedin_templates/template_1.png',
-                    templateName: 'Text Post',
-                    templateID: '1',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/linkedin_templates/template_2.png',
-                    templateName: 'Media Post',
-                    templateID: '2',
-                    description: "description"
-                },
-                {
-                    templateImageURL: '/images/linkedin_templates/template_3.png',
-                    templateName: 'Multiple Media Post',
-                    templateID: '3',
-                    description: "description"
-                }])
-            }
-        }
+        getTemplates()
     }, [])
 
     const getTemplates = async () => {
