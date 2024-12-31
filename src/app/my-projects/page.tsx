2e132f5b07c87b373b8258df30ec85853a0ec2ef
Khang Lam <lamchikhang.it@gmail.com>
2024-12-31 12:18:36 +0700

    fix bug

--- src/app/my-projects/page.tsx
+++ src/app/my-projects/page.tsx
@@ -1,59 +1,57 @@
 'use client'
 import { FC, useState } from "react"
-import { GridIcon, ListIcon } from "@/assets/icons/AppIcons"
-import Breadcrumb from "@/components/global/Breadcrumb"
-import Table from "@/components/global/Table"
-import LayoutWrapper from "@/layout/LayoutWrapper"
-import MyProjectCard from "@/components/wrapper/MyProjectCard"
 import { useRouter } from "next/navigation"
 import ProjectPageLayout from "@/layout/specific_layout/ProjectPageLayout"
 
-  
-const MyProjects:FC = () => {
+const MyProjects: FC = () => {
   const router = useRouter()
-  const [isList,setIsList] = useState<Boolean>(true)
+  const [isList, setIsList] = useState<Boolean>(true)
 
   const tableData = [
     {
       projectName: 'GreenLake',
       creadedOn: '18.01.2024',
       approvedOn: '20.01.2024',
-      
+      dataClick: 'GreenLake'
     },
     {
       projectName: 'Lorem ipsum dolor sit amet',
       creadedOn: '18.01.2024',
       approvedOn: '20.01.2024',
+      dataClick: 'Lorem ipsum dolor sit amet',
     },
     {
       projectName: 'Lorem ipsum dolor sit amet',
       creadedOn: '21.01.2024',
       approvedOn: '22.01.2024',
+      dataClick: 'Lorem ipsum dolor sit amet',
     },
     {
       projectName: 'Lorem ipsum dolor sit amet',
       creadedOn: '21.01.2024',
       approvedOn: '22.01.2024',
-      
-    },
-    {
-      projectName: 'Lorem ipsum dolor sit amet',
-      creadedOn: '21.01.2024',
-      approvedOn: '22.01.2024',  
+      dataClick: 'Lorem ipsum dolor sit amet',
     },
     {
       projectName: 'Lorem ipsum dolor sit amet',
       creadedOn: '21.01.2024',
       approvedOn: '22.01.2024',
+      dataClick: 'Lorem ipsum dolor sit amet',
+    },
+    {
+      projectName: 'Lorem ipsum dolor sit amet',
+      creadedOn: '21.01.2024',
+      approvedOn: '22.01.2024',
+      dataClick: 'Lorem ipsum dolor sit amet',
     }
-];
-    
+  ];
 
-    const toggleListType = () => {
-      setIsList(pre=>!pre)
-    }
 
-  const onSelectingProject = (project_name:string) => {
+  const toggleListType = () => {
+    setIsList(pre => !pre)
+  }
+
+  const onSelectingProject = (project_name: string) => {
     console.log('project_name **', project_name);
     router.push(`my-projects/${encodeURIComponent(project_name)}`)
   }
@@ -61,11 +59,11 @@ const MyProjects:FC = () => {
   const tableHeading = ["Project Name", "Created On", "Last Edited"]
 
   const arrowshowItems = ["Project Name", "Created On", "Last Edited"]
- 
+
 
   return (
     <>
-        <ProjectPageLayout viewType="project" project_data={tableData} onSelectingProjects={onSelectingProject} tableHeadings={tableHeading} headersHavingToggle={arrowshowItems} page="My Project"/>
+      <ProjectPageLayout fieldClick="dataClick" viewType="project" project_data={tableData} onSelectingProjects={onSelectingProject} tableHeadings={tableHeading} headersHavingToggle={arrowshowItems} page="My Project" />
     </>
   )
 }
