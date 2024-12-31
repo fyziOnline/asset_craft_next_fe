e1de6769d12fa9412684742c9946d431f65d12ff
Khang Lam <lamchikhang.it@gmail.com>
2024-12-31 14:01:18 +0700

    fix ui

--- src/app/my-projects/[project_name]/[campaign_name]/[type_page]/asset-generate/LinkedinPage.tsx
+++ src/app/my-projects/[project_name]/[campaign_name]/[type_page]/asset-generate/LinkedinPage.tsx
@@ -1,49 +1,66 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Accordion from '@/components/global/Accordion';
 import Button from '@/components/global/Button';
 import TextField from '@/components/global/TextField';
 import DropDown from '@/components/global/DropDown';
 import ChildrenTitle from '@/components/global/ChildrenTitle';
 import RangeSlider from '@/components/global/RangeSlider';
-import { useRouter } from 'next/navigation';
 import DragAndDrop from '@/components/global/DragAndDrop';
 import { useAppData } from '@/context/AppContext';
+import { AssetHtmlProps, Template } from '@/types/templates';
+import { useGenerateTemplate } from '@/hooks/useGenerateTemplate';
+import { FormDataProps, SectionProps, useInputFormDataGenerate } from '@/hooks/useInputFormDataGenerate';
+import { useLoading } from '@/components/global/Loading/LoadingContext';
 
-const LinkedInPage = () => {
-    const [isOpen, setIsOpen] = useState(false);
+interface LinkedInPageProps {
+    params: {
+        template: Template
+        project_name?: string
+    }
+}
+
+const ListTargetAudience = [
+    { label: 'General Public', value: 'General Public' },
+    { label: 'Existing Customers', value: 'Existing Customers' },
+    { label: 'Prospective Customers', value: 'Prospective Customers' }
+]
+
+const listofcampains = [
+    { label: 'Product Launch', value: 'Product Launch' },
+    { label: 'Event Promotion', value: 'Event Promotion' },
+    { label: 'Brand Awareness', value: 'Brand Awareness' },
+    { label: 'Demand Generation', value: 'Demand Generation' }
+]
+
+const emailType = [
+    { label: 'Announcement', value: 'Announcement' },
+    { label: 'Newsletter', value: 'Newsletter' },
+    { label: 'Promotional Email', value: 'Promotional Email' }
+]
+
+const keyPoints = [
+    { label: 'Innovation', value: 'Innovation' },
+    { label: 'Performance', value: 'Performance' },
+    { label: 'Increased Security', value: 'Increased Security' },
+]
+
+const LinkedInPage = ({ params }: LinkedInPageProps) => {
     const [generateStep, setGenerateStep] = useState(1); //1 - Normal, 2 - (Loading or disable), 3 - Regenerate
     const [checkedList, setCheckedList] = useState<number[]>([]);
     const [disableList, setDisableList] = useState<number[]>([2, 3, 4]);
     const [isShowList, setIsShowList] = useState<number[]>([]);
+    const { generateHTML } = useGenerateTemplate({ params: { templateID: params.template?.templateID ?? '' as string } })
+    const { refFormData, refSection, handleInputText, handleInputSection } = useInputFormDataGenerate()
+    const { setShowLoading } = useLoading()
+    const { contextData, setContextData } = useAppData();
 
-    const { setContextData } = useAppData();
-
-
-    const ListTargetAudience = [
-        { label: 'General Public', value: 'General Public' },
-        { label: 'Existing Customers', value: 'Existing Customers' },
-        { label: 'Prospective Customers', value: 'Prospective Customers' }
-    ]
-
-    const listofcampains = [
-        { label: 'Product Launch', value: 'Product Launch' },
-        { label: 'Event Promotion', value: 'Event Promotion' },
-        { label: 'Brand Awareness', value: 'Brand Awareness' },
-        { label: 'Demand Generation', value: 'Demand Generation' }
-    ]
-
-    const emailType = [
-        { label: 'Announcement', value: 'Announcement' },
-        { label: 'Newsletter', value: 'Newsletter' },
-        { label: 'Promotional Email', value: 'Promotional Email' }
-    ]
-
-    const keyPoints = [
-        { label: 'Innovation', value: 'Innovation' },
-        { label: 'Performance', value: 'Performance' },
-        { label: 'Increased Security', value: 'Increased Security' },
-    ]
+    useEffect(() => {
+        refFormData.current = {
+            ...refFormData.current,
+            product: params.project_name
+        }
+    }, [])
 
     const onNext = (step: number): void => {
         if (step === 1) {
@@ -77,66 +94,92 @@ const LinkedInPage = () => {
         }
     };
 
-
-    const handleGenerate = (): void => {
+    const handleGenerate = async () => {
         if (generateStep === 2 || checkedList.length !== 4) {
             return;
         }
 
         let newStep = generateStep + 1;
 
-        if (newStep === 5) { // Reset after completing step 4
+        if (newStep > 3) { // Reset after completing step 3
             newStep = 1;
-            setIsOpen(false);
             setIsShowList([]);
             setCheckedList([]);
             setDisableList([2, 3, 4]);
+            setContextData({ assetTemplateShow: false });
         } else {
+            setContextData({ assetTemplateShow: true });
+
             if (newStep === 2) {
                 setCheckedList([1, 2, 3, 4]);
                 setDisableList([1, 2, 3, 4]);
-                setTimeout(() => {
-                    setGenerateStep(3);
-                    setContextData({ assetGenerateStatus: 3 });
-                }, 3000);
+                setContextData({ assetGenerateStatus: newStep });
+                setGenerateStep(newStep);
+                setShowLoading(true)
+                const res = await generateHTML(refFormData.current as FormDataProps, refSection.current as SectionProps[], contextData.isRegenerateHTML)
+                setShowLoading(false)
+                setGenerateStep(3);
+                setContextData({ assetGenerateStatus: 3, AssetHtml: res as AssetHtmlProps, isShowEdit_Save_Button: res?.isSuccess, isRegenerateHTML: true });
+                return
             }
-            setIsOpen(true);
-            setContextData({ assetTemplateShow: true });
         }
         setContextData({ assetGenerateStatus: newStep });
         setGenerateStep(newStep);
     };
-
 
     return (
         <div>
             <div className='mt-[40px]'>
                 {/* step 1 */}
                 <Accordion
+                    isRequire={true}
                     HeaderTitle="Campaign Overview"
                     checked={checkedList.includes(1)}
                     disableShowContent={disableList.includes(1)}
                     handleShowContent={() => { setIsShowList([1]) }}
                     isShowContent={isShowList.includes(1)}>
-                    <div className='max-w-[90%]'>
-                        <ChildrenTitle title='Product/Solution' ></ChildrenTitle>
-                        <TextField placeholder="Enter the name of the product or solution." customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
+                    <div>
+                        <ChildrenTitle title='Product/Solution'></ChildrenTitle>
+                        <TextField
+                            handleChange={(e) => { handleInputText(e, "product") }}
+                            placeholder="Enter the name of the product or solution."
+                            value={params.project_name}
+                            customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
 
                         <div className='flex items-center gap-[16%]'>
-                            <div>
+                            <div className='w-[260px]'>
                                 <ChildrenTitle title='Campaign Goal' customClass='mt-5' ></ChildrenTitle>
-                                <DropDown selectPlaceHolder="Select Campaign Goal" optionLists={listofcampains} ></DropDown>
+                                <DropDown
+                                    onSelected={(optionSelected) => {
+                                        refFormData.current = {
+                                            ...refFormData.current,
+                                            campaignGoal: optionSelected.value
+                                        }
+                                    }}
+                                    selectPlaceHolder="Select Campaign Goal" optionLists={listofcampains} ></DropDown>
                             </div>
 
-                            <div>
+                            <div className='w-[260px]'>
                                 <ChildrenTitle title='Target audience' customClass='mt-5' ></ChildrenTitle>
-                                <DropDown selectPlaceHolder="Select Target Audience" optionLists={ListTargetAudience} ></DropDown>
+                                <DropDown
+                                    onSelected={(optionSelected) => {
+                                        refFormData.current = {
+                                            ...refFormData.current,
+                                            targetAudience: optionSelected.value
+                                        }
+                                    }}
+                                    selectPlaceHolder="Select Target Audience" optionLists={ListTargetAudience} ></DropDown>
                             </div>
                         </div>
 
                         <div className='w-[300px]'>
                             <ChildrenTitle title='How creative you want the output?' customClass='mt-5' ></ChildrenTitle>
-                            <RangeSlider></RangeSlider>
+                            <RangeSlider onSelectValue={(value) => {
+                                refFormData.current = {
+                                    ...refFormData.current,
+                                    outputScale: value
+                                }
+                            }}></RangeSlider>
                         </div>
                     </div>
                     <div className='max-w-full flex justify-end pt-5 pb-3'>
@@ -155,6 +198,7 @@ const LinkedInPage = () => {
             <div className='mt-[25px]'>
                 {/* step 2 */}
                 <Accordion
+                    isRequire={true}
                     HeaderTitle="Post Context"
                     checked={checkedList.includes(2)}
                     disableShowContent={disableList.includes(2)}
@@ -162,17 +206,29 @@ const LinkedInPage = () => {
                     isShowContent={isShowList.includes(2)}>
                     <div className='max-w-[90%]'>
                         <ChildrenTitle customClass='mt-5' title='Specify the topic, occasion, event or context for your post.' />
-                        <TextField placeholder="Please enter the name of your campaign, event or occasion." customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
+                        <TextField
+                            handleChange={(e) => { handleInputText(e, "topic") }}
+                            placeholder="Please enter the name of your campaign, event or occasion." customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
 
                         <div className='flex items-center gap-[16%]'>
-                            <div>
+                            <div className='w-[260px]'>
                                 <ChildrenTitle title='Email Type' customClass='mt-5' />
-                                <DropDown selectPlaceHolder="Select Post Type" optionLists={emailType} />
+                                <DropDown onSelected={(optionSelected) => {
+                                    refFormData.current = {
+                                        ...refFormData.current,
+                                        type: optionSelected.value
+                                    }
+                                }} selectPlaceHolder="Select Post Type" optionLists={emailType} />
                             </div>
 
-                            <div>
+                            <div className='w-[260px]'>
                                 <ChildrenTitle title='Key Points' customClass='mt-5' />
-                                <DropDown selectPlaceHolder="Select Key Points" optionLists={keyPoints} />
+                                <DropDown onSelected={(optionSelected) => {
+                                    refFormData.current = {
+                                        ...refFormData.current,
+                                        keyPoints: optionSelected.value
+                                    }
+                                }} selectPlaceHolder="Select Key Points" optionLists={keyPoints} />
                             </div>
                         </div>
 
@@ -181,9 +237,9 @@ const LinkedInPage = () => {
                         <Button
                             buttonText='Back'
                             showIcon
-                            textStyle='text-[1rem] font-base text-[#00A881]'
-                            textColor="text-[#B1B1B1]"
-                            iconColor="#B1B1B1"
+                            textStyle='text-[1rem] font-base text-[#000000]'
+                            textColor="text-[#000000]"
+                            iconColor="#000000"
                             backgroundColor='bg-[#fff]'
                             customClassIcon="rotate-180"
                             handleClick={() => { onBack(2) }}
@@ -209,18 +265,24 @@ const LinkedInPage = () => {
                     handleShowContent={() => { setIsShowList([3]) }}
                     isShowContent={isShowList.includes(3)}>
                     <div>
-                        <DragAndDrop />
+                        <DragAndDrop onFileSelect={(file) => {
+                            refFormData.current = {
+                                ...refFormData.current,
+                                fileSelected: file
+                            }
+                        }} />
 
                         <ChildrenTitle customClass='mt-5' title='Website Link'></ChildrenTitle>
-                        <TextField placeholder="Paste your URL here." customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
+                        <TextField handleChange={(e) => { handleInputText(e, "webUrl") }}
+                            placeholder="Paste your URL here." customAreaClass='whitespace-nowrap overflow-x-auto overflow-y-hidden scrollbar-hide'></TextField>
                     </div>
                     <div className='max-w-full flex justify-end pt-5 pb-3'>
                         <Button
                             buttonText='Back'
                             showIcon
-                            textStyle='text-[1rem] font-base text-[#00A881]'
-                            textColor="text-[#B1B1B1]"
-                            iconColor="#B1B1B1"
+                            textStyle='text-[1rem] font-base text-[#000000]'
+                            textColor="text-[#000000]"
+                            iconColor="#000000"
                             backgroundColor='bg-[#fff]'
                             customClassIcon="rotate-180"
                             handleClick={() => { onBack(3) }}
@@ -247,6 +309,24 @@ const LinkedInPage = () => {
                     handleShowContent={() => { setIsShowList([4]) }}
                     isShowContent={isShowList.includes(4)}>
                     <div>
+                        {params.template?.templatesBlocks && params.template?.templatesBlocks.filter((item) => !item.isStatic).map((item, index) => {
+                            if (params.template.templatesBlocks && refSection.current.length < params.template.templatesBlocks.length) {
+                                refSection.current = [...refSection.current as SectionProps[], {
+                                    templateBlockID: item.templateBlockID || "",
+                                    aiPrompt: item.aiPrompt || ""
+                                }]
+                            }
+
+                            return (
+                                <div key={index}>
+                                    <ChildrenTitle title={`Section ${index + 1}: ${item.aiTitle || ''}`} customClass={`text-[18px] ${index === 0 ? "" : "mt-[20px]"}`} />
+                                    <ChildrenTitle title={item.aiDescription || ''} customClass="text-[14px]" />
+                                    <TextField handleChange={(e) => { handleInputSection(e, index) }} customClass='h-16' defaultValue={item.aiPrompt || ''} />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    {/* <div>
                         <ChildrenTitle title='Hook/Headline & Introduction' customClass="text-[18px]" />
                         <ChildrenTitle title='Catchy introduction to the topic/product along with a brief overview.' />
                         <TextField customClass='h-16' placeholder={`"Write a compelling headline that promotes the benefits of HPE GreenLake’s hybrid cloud solution, followed by a 2-3 sentence introduction explaining the benefits of HPE GreenLake and why attending StorageAsia 2024 is beneficial."`} />
@@ -259,14 +339,14 @@ const LinkedInPage = () => {
                         <ChildrenTitle title='Industry-relevant hashtags for discoverability.' />
                         <TextField placeholder={`"Generate 4-5 relevant hashtags for an HPE GreenLake LinkedIn post on hybrid cloud solutions."`} rows={1} />
 
-                    </div>
+                    </div> */}
                     <div className='max-w-full flex justify-end pt-5 pb-3'>
                         <Button
                             buttonText='Back'
                             showIcon
-                            textStyle='text-[1rem] font-base text-[#00A881]'
-                            textColor="text-[#B1B1B1]"
-                            iconColor="#B1B1B1"
+                            textStyle='text-[1rem] font-base text-[#000000]'
+                            textColor="text-[#000000]"
+                            iconColor="#000000"
                             backgroundColor='bg-[#fff]'
                             customClassIcon="rotate-180"
                             handleClick={() => { onBack(4) }}
