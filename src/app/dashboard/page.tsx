64aa428053eeb770680083809540774dd13dc225
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-01-07 10:05:16 +0530

    dashboard updates

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -126,7 +126,7 @@ const Dashboard: FC = () => {
         </div>
       </ProjectSetUpModal>
 
-      <div className="px-8 p-4">
+      {/* <div className="px-8 p-4">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl tracking-wide text-green-100 font-bold leading-normal">
           Welcome, {userDetails?.name}
@@ -134,7 +134,7 @@ const Dashboard: FC = () => {
           <SearchBox />
         </div>
         <p className="text-base font-bold tracking-wide">Overview:</p>
-      </div>
+      </div> */}
       <div className="px-8 flex items-center gap-9">
         {updatedDashboardData.map((data, index) => (
           <DashboardCard
@@ -149,7 +149,7 @@ const Dashboard: FC = () => {
       </div>
 
       <div className="pl-8 pt-5 flex w-full mb-32">
-        <div className="w-[70%] border-t border-[#D9D9D9]">
+        <div className="w-[70%] border-[#D9D9D9]">
           <div className="mt-5">
             <p className="text-lg font-bold tracking-wide">
               What would you like to create today?
@@ -162,11 +162,11 @@ const Dashboard: FC = () => {
                   buttonText={item.assetTypeName}
                   showIcon={false}
                   IconComponent={item.assetTypeName === "All in One" && <ExpressIcon strokeColor="white" width="40" height="38" />}
-                  backgroundColor={"bg-white"}
-                  customClass={"px-12 py-1 border-2 border-green-300 min-w-min px-[50px]"}
-                  textColor={"text-foreground"}
+                  backgroundColor="bg-white"
+                  customClass="group px-12 py-2 border border-green-300 min-w-min px-[50px] transition-all duration-300 hover:bg-[#073634]"
+                  textColor="text-foreground group-hover:text-white"
                   handleClick={() => handleShowPopup(item)}
-                  textStyle={`font-normal text-sm text-center whitespace-nowrap`}
+                  textStyle="font-normal text-sm text-center whitespace-nowrap"
                 />
               ))}
             </div>
@@ -188,14 +188,14 @@ const Dashboard: FC = () => {
         <div className="w-[30%] bg-[#F9F9F9] rounded-[14px] ml-4">
           <div className="p-6">
             <div className="flex items-baseline justify-between">
-              <p className="text-lg font-bold">Pending Approval:</p>
+              <p className="text-lg font-bold">Pending Approval</p>
               <p className="text-base font-normal underline">View all</p>
             </div>
             {pendingApprovals.map((data, index) => (
-              <div key={index} className="rounded-[15px] border-2 border-[#00A881] bg-white p-3 mt-2">
+              <div key={index} className={`rounded-[15px] border p-3 mt-2 ${index % 2 ===0 ? 'bg-white' : 'bg-[#EFEFEF]'}`}>
                 <p className="text-[##2F363F] font-inter text-base font-normal mb-1">{data.name}</p>
-                <div className="flex items-center justify-between">
-                  <p className="text-[#636363] font-semibold text-sm">Last Updated:</p>
+                <div className="flex items-center justify-between mt-2">
+                  <p className="text-[#636363] font-semibold text-sm">Last Updated :</p>
                   <p className="text-[#636363] text-sm font-normal">{data.lastUpdated}</p>
                 </div>
               </div>
--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,36 @@
 import { UserIcon } from "@/assets/icons/AppIcons"
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import SearchBox from "@/components/global/SearchBox";
 
-const Header:React.FC = () => {
-  return (
-    <header className="fixed top-0 right-0 left-[5.4rem] bg-white p-[0.6rem] z-50">
-        <div className="w-full flex justify-end">
+const Header: React.FC = () => {
+  const pathname = usePathname()
+
+  if (pathname === "/dashboard") {
+    return (
+      <header className="fixed top-0 right-0 left-[5.4rem] bg-white p-[0.6rem] z-50 border-b">
+        <div className="w-full flex items-center justify-between px-8">
+          <div className="w-full flex items-center justify-between">
+            <div className="flex items-center gap-4">
+              <h1 className="text-xl font-bold">Dashboard</h1>
+              <SearchBox />
+            </div>
+
+            <div>
+              <h3>Welcome, Tom</h3>
+            </div>
+          </div>
           <Link href="/Profile" className="cursor-pointer"><UserIcon /></Link>
         </div>
+      </header>
+    )
+  }
+
+  return (
+    <header className="fixed top-0 right-0 left-[5.4rem] bg-white p-[0.6rem] z-50">
+      <div className="w-full flex justify-end">
+        <Link href="/Profile" className="cursor-pointer"><UserIcon /></Link>
+      </div>
     </header>
   )
 }
