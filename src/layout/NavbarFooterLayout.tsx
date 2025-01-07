3bca31bcaca8713c2e22f757452b3c2ef3b0cace
Muhammad Fyrooz Razi C <151521733+FyroozStrat@users.noreply.github.com>
2025-01-07 19:19:07 +0530

    build error fix

--- src/layout/NavbarFooterLayout.tsx
+++ src/layout/NavbarFooterLayout.tsx
@@ -1,5 +1,5 @@
-import Footer from "@/components/layout/Footer";
-import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/Layout/Footer";
+import Navbar from "@/components/Layout/Navbar";
 
 interface LayoutProps {
     children: React.ReactNode;
