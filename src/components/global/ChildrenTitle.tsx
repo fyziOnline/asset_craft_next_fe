1da36deb2021d3debff0fdbefecb1d7fa8a6ee25
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-01-13 19:35:46 +0530

    ui bugs fixed

--- src/components/global/ChildrenTitle.tsx
+++ src/components/global/ChildrenTitle.tsx
@@ -4,10 +4,11 @@ import React from 'react';
 interface ChildrenTitleProps {
     title: string;
     customClass?: string;
+    showStar?: boolean;
 }
 
-const ChildrenTitle: React.FC<ChildrenTitleProps> = ({ title = "", customClass = "" }) => {
-    return (<p className={`font-semibold text-[#160647] text-base tracking-[0] leading-5 text-wrap whitespace-nowrap mb-[12px] ${customClass}`}>{title}</p>)
+const ChildrenTitle: React.FC<ChildrenTitleProps> = ({ title = "", customClass = "", showStar = false }) => {
+    return (<p className={`font-semibold text-[#160647] text-base tracking-[0] leading-5 text-wrap whitespace-nowrap mb-[12px] ${customClass}`}>{title} {showStar && <span className="text-red-500">*</span>}</p>)
 }
 
 export default ChildrenTitle;
