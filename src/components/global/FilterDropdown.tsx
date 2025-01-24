9fb68bd533ae6902732fe1259bc07371b7206b22
rahulstratagile <rahul.v@stratagile.com>
2025-01-24 09:52:01 +0530

    created filter dropdown component

---
+++ src/components/global/FilterDropdown.tsx
@@ -0,0 +1,61 @@
+import React, { FC, useEffect, useRef, useState } from 'react'
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+
+export interface DropDownOptions {
+    label: string;
+    value: string;
+}
+
+interface FilterDropdownProps {
+    customClass?: string;
+    optionLists: DropDownOptions[];
+    placeholder?: string
+}
+
+const FilterDropdown: FC<FilterDropdownProps> = ({ customClass, optionLists, placeholder = 'Select' }) => {
+    const [showOptionList, setShowOptionList] = useState(false)
+    const [selectedOption, setSelectedOption] = useState('')
+    const [selectedLabel, setSelectedLabel] = useState('')
+
+    const dropdownRef = useRef<HTMLDivElement | null>(null)
+
+    const handleDropDownList = () => {
+        setShowOptionList((prev) => !prev)
+    }
+
+    const handleSelectList = (options: DropDownOptions) => {
+        setSelectedOption(options.value)
+        setSelectedLabel(options.label)
+        setShowOptionList(false)
+    }
+
+    useEffect(() => {
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setShowOptionList(false)
+            }
+        }
+        document.addEventListener('mousedown', handleOutsideClick)
+        return () => {
+            document.removeEventListener('mousedown', handleDropDownList)
+        }
+    }, [])
+
+    return (
+        <div ref={dropdownRef} className='relative'>
+            <div onClick={handleDropDownList} className={`flex items-center justify-between gap-2 rounded-full w-[200px] border border-[rgba(217,217,217,0.93)] px-3 py-1 cursor-pointer ${customClass}`} >
+                <p className={`text-base tracking-wide ${!selectedOption ? "text-[#666666]" : "" }`}>{selectedLabel || placeholder}</p>
+                <span className={`cursor-pointer transition-transform ${showOptionList ? "rotate-180" : ""}`}><MdOutlineKeyboardArrowDown color='#00A881' size={25} /></span>
+            </div>
+            {showOptionList &&
+                <div className='z-40 bg-[#F6F6F6] border border-[rgba(217,217,217,0.93)] py-2 mt-2 absolute h-auto w-[200px] flex flex-col shadow-dropdown-shadow rounded-2xl transition-all duration-300 ease-in-out' >
+                    {optionLists.map((options, index) => (
+                        <div key={index} onClick={() => handleSelectList(options)} className='h-4 px-3 py-4 flex items-center text-base cursor-pointer'>{options.label}</div>
+                    ))}
+                </div>
+            }
+        </div>
+    )
+}
+
+export default FilterDropdown
--- src/layout/specific_layout/AssetsPageLayout.tsx
+++ src/layout/specific_layout/AssetsPageLayout.tsx
@@ -6,6 +6,8 @@ import { GridIcon, ListIcon } from '@/assets/icons/AppIcons'
 import Table from '@/components/global/Table'
 import Title from '@/components/global/Title'
 import SearchBox from "@/components/global/SearchBox";
+import DropDown from '@/components/global/DropDown'
+import FilterDropdown from '@/components/global/FilterDropdown'
 
 
 interface Asset {
@@ -22,25 +24,37 @@ interface AssetsPageProps {
   columnWidthsTable?: string[]
   handleClick?: (value: any) => void;
   page: string
-  isIconRequired ?: boolean
+  isIconRequired?: boolean
 }
 
-const AssetsPageLayout: FC<AssetsPageProps> = ({ campaign_data, tableHeadings, headersHavingToggle, hiddenFields = [], page, handleClick, columnWidthsTable = [],isIconRequired = true }) => {
+const AssetsPageLayout: FC<AssetsPageProps> = ({ campaign_data, tableHeadings, headersHavingToggle, hiddenFields = [], page, handleClick, columnWidthsTable = [], isIconRequired = true }) => {
   const [isList, setIsList] = useState<Boolean>(true)
   const [searchQuery, setSearchQuery] = useState<string>('')
-  
+
   const toggleListType = () => {
     setIsList(pre => !pre)
   }
+
+  const filterOptionsAsset = [
+    { label: 'Email', value: 'Email' },
+    { label: 'LinkedIn', value: 'LinkedIn' },
+    { label: 'Landing Page', value: 'Landing Page' },
+    { label: 'Call Script', value: 'Call Script' }
+  ]
+
+  const filterOptionsStatus = [
+    { label: 'In Progress', value: 'In Progress' },
+    { label: 'On Review', value: 'On Review' },
+  ]
 
   const filteredData = useMemo(() => {
     if (!searchQuery.trim()) return campaign_data;  // Show all data if search is empty
 
     return campaign_data.filter((item) => {
       const projectName = item['projectName']?.toLowerCase() || '';
-    const campaignName = item['campaignName']?.toLowerCase() || '';
-    const assetName = item['assetName']?.toLowerCase() || '';
-    const query = searchQuery.toLowerCase();
+      const campaignName = item['campaignName']?.toLowerCase() || '';
+      const assetName = item['assetName']?.toLowerCase() || '';
+      const query = searchQuery.toLowerCase();
 
       return (
         projectName.includes(query) ||
@@ -49,21 +63,25 @@ const AssetsPageLayout: FC<AssetsPageProps> = ({ campaign_data, tableHeadings, h
       )
     })
   }, [searchQuery, campaign_data])
-  
 
-  
+
+
 
   return (
     <>
       <div className="flex items-center justify-between pt-[1rem] px-[1.5rem] mt-5">
 
-      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-5">
 
-        {/* <Breadcrumb TaskType={page} /> */}
-        <Title titleName={page} />
+          {/* <Breadcrumb TaskType={page} /> */}
+          <Title titleName={page} />
 
-        <SearchBox customClass="bg-[#F6F6F6]" setSearchQuery={setSearchQuery}/>
-          
+          <div className='flex items-center gap-4'>
+            <SearchBox customClass="bg-[#F6F6F6]" setSearchQuery={setSearchQuery} />
+            <FilterDropdown placeholder='Select Asset Type' optionLists={filterOptionsAsset} customClass="bg-[#F6F6F6]" />
+            <FilterDropdown placeholder='Select Status' optionLists={filterOptionsStatus} customClass="bg-[#F6F6F6]" />
+          </div>
+
         </div >
 
         <span className="pr-10 cursor-pointer" onClick={toggleListType}>{!isList ? <ListIcon /> : <GridIcon />}</span>
