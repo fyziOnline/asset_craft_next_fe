c2ff3fcb3981cde071d065a7da7841f01ea9fa65
rahulstratagile <rahul.v@stratagile.com>
2025-01-28 17:47:37 +0530

    loader added

--- src/components/global/Loading/LoadingOverlay.tsx
+++ src/components/global/Loading/LoadingOverlay.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Player } from '@lottiefiles/react-lottie-player'
+import loadingAnimation from '../../../../public/loadingIcon.json'
 
 const LoadingOverlay: React.FC<{ loading: boolean }> = ({ loading }) => {
     if (!loading) return null;
 
     return (
-        <div className="fixed inset-0 bg-black opacity-40 flex justify-center items-center z-[1000] pointer-events-none">
-            <div className="border-t-4 border-[#00A881] border-solid w-16 h-16 rounded-full animate-spin pointer-events-none"></div>
+        <div className="fixed inset-0 bg-[#d0cece54] backdrop-blur-[2px] flex justify-center items-center z-[1000] pointer-events-none">
+            <div className='pointer-events-none'>
+                <Player
+                    src={loadingAnimation}
+                    loop
+                    autoplay
+                    style={{ height: '500px', width: '500px' }}
+                />
+            </div>
         </div>
     );
 };
