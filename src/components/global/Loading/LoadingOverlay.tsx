53c983c9f8192642d6d9bd0e109ac36726e62bb7
rahulstratagile <rahul.v@stratagile.com>
2025-01-28 16:45:10 +0530

    fix navbar hovering issue when loading

--- src/components/global/Loading/LoadingOverlay.tsx
+++ src/components/global/Loading/LoadingOverlay.tsx
@@ -4,8 +4,8 @@ const LoadingOverlay: React.FC<{ loading: boolean }> = ({ loading }) => {
     if (!loading) return null;
 
     return (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-45 flex justify-center items-center z-[1000]">
-            <div className="border-t-4 border-[#00A881] border-solid w-16 h-16 rounded-full animate-spin"></div>
+        <div className="fixed inset-0 bg-black opacity-40 flex justify-center items-center z-[1000] pointer-events-none">
+            <div className="border-t-4 border-[#00A881] border-solid w-16 h-16 rounded-full animate-spin pointer-events-none"></div>
         </div>
     );
 };
