c73597821ea49fa724ae3d149ed562aafdaa6748
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-02-19 15:20:40 +0530

    login screen make mobile responsive

--- src/components/Layout/HomeHeader.tsx
+++ src/components/Layout/HomeHeader.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react'
 
 const HomeHeader:FC = () => {
   return (
-    <header className='px-32'>
+    <header className='px-8 md:px-20 lg:px-32'>
         <HPE_BRAND_Logo className='flex justify-start' width={150} height={60}/>
     </header>
   )
