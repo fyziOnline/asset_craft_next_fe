25cc4c00c56f62d0625bb635a569b21bc813636d
Simon Philip <simon.p@stratagile.com>
2025-02-21 11:03:36 +0800

    change imagepicker to use keys in json

--- src/app/edit-html-content/components/Controller/test/ImageController.ts
+++ src/app/edit-html-content/components/Controller/test/ImageController.ts
@@ -1,6 +1,10 @@
-import { rankWith, scopeEndsWith, RankedTester } from '@jsonforms/core';
+import { rankWith, or, formatIs, optionIs, RankedTester } from '@jsonforms/core';
 
 export const ImagePickerTester: RankedTester = rankWith(
   5,
-  scopeEndsWith("image_url")
+  or(
+    formatIs('imageUrl'),
+    // You can also check for a custom option in uiSchema
+    optionIs('widget', 'imagePicker')
+  )
 );
