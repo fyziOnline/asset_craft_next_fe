784c70e52ecb2211e47dd39d6f65685835e9fb6a
rahulstratagile <rahul.v@stratagile.com>
2025-02-21 14:40:24 +0530

    visible asset name display on card

--- src/components/global/CardContent.tsx
+++ src/components/global/CardContent.tsx
@@ -1,16 +1,18 @@
 import { FC } from "react"
 
 interface ContentProp {
-    header : string
-    content : string
-    cardClass ?: string
+  header: string
+  content: string
+  subcontent?: string
+  cardClass?: string
 }
 
-const CardContent:FC<ContentProp>= ({header,content,cardClass}) => {
+const CardContent: FC<ContentProp> = ({ header, content, cardClass, subcontent }) => {
   return (
     <section className={`${cardClass}`}>
-        <h2 className="font-medium text-[1rem] md:text-[1.3rem]  text-grey-800 group-hover:text-white mb-1">{header}</h2>
-        <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{content}</p>
+      <h2 className="font-medium text-[1rem] md:text-[1.3rem]  text-grey-800 group-hover:text-white mb-1">{header}</h2>
+      <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{content}</p>
+      <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{subcontent}</p>
     </section>
   )
 }
