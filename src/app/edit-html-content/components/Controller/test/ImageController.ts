7eebe1e95d8033655ea4cab12f0cc783a9f6ffc1
Simon Philip <simon.p@stratagile.com>
2025-02-21 11:40:27 +0800

    add small case options as well.

--- src/app/edit-html-content/components/Controller/test/ImageController.ts
+++ src/app/edit-html-content/components/Controller/test/ImageController.ts
@@ -3,8 +3,8 @@ import { rankWith, or, formatIs, optionIs, RankedTester } from '@jsonforms/core'
 export const ImagePickerTester: RankedTester = rankWith(
   5,
   or(
-    formatIs('imageUrl'),
+    formatIs('imageUrl'), formatIs('imageurl'),
     // You can also check for a custom option in uiSchema
-    optionIs('widget', 'imagePicker')
+    optionIs('widget', 'imagePicker'), optionIs('widget', 'imagepicker'),
   )
 );
