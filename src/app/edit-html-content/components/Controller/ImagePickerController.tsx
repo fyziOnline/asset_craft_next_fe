51493674b4bd7f5c61aa9454a809eaa80c2a84be
Muhammad Fyrooz Razi C <151521733+FyroozStrat@users.noreply.github.com>
2025-02-24 13:56:36 +0530

    show images based on orientation in ui schema

--- src/app/edit-html-content/components/Controller/ImagePickerController.tsx
+++ src/app/edit-html-content/components/Controller/ImagePickerController.tsx
@@ -3,17 +3,23 @@ import { withJsonFormsControlProps } from '@jsonforms/react';
 import { ControlProps } from '@jsonforms/core';
 import { ImagePicker } from './ImagePicker';
 
+// interface CustomControlProps extends ControlProps {
+//   uischema :
+// }
+
 const ImagePickerControl = ({
   data,
   handleChange,
   path,
   label,
+  uischema,
   required
 }: ControlProps) => (
    <ImagePicker
     value={data || ''}
     onChange={(newValue: string ) => handleChange(path, newValue)}
     label={`${label}${required ? ' *' : ''}`}
+    uischema={uischema}
   />
 );
 
---
+++ src/app/edit-html-content/components/Controller/components/dialogue-body/SingleImageReviewer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react"
+
+const SingleImageReviewer:FC = () => {
+  return (
+    <div>SingleImageReviewer</div>
+  )
+}
+
+export default SingleImageReviewer
