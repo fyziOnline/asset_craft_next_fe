53d5aeb01843b5280359123dfe5d7650ab651086
rahulstratagile <rahul.v@stratagile.com>
2025-02-26 10:39:54 +0530

    icons asset error fixed

--- src/components/global/CardContent.tsx
+++ src/components/global/CardContent.tsx
@@ -11,8 +11,8 @@ const CardContent: FC<ContentProp> = ({ header, content, cardClass, subcontent }
   return (
     <section className={`${cardClass}`}>
       <h2 className="font-medium text-[1rem] md:text-[1.3rem]  text-grey-800 group-hover:text-white mb-1">{header}</h2>
-      <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{content}</p>
-      <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{subcontent}</p>
+      <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{subcontent} | {content}</p>
+      {/* <p className="text-xs sm:text-[1rem] leading-6 xl:leading-7 text-grey-500 group-hover:text-white line-clamp-3">{subcontent}</p> */}
     </section>
   )
 }
