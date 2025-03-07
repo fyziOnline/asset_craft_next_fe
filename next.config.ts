b2df6c7386e415a9d868dc1096a8184eb6fe5857
Simon Philip <simon.p@stratagile.com>
2025-03-07 12:36:45 +0800

    add env variables in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,10 @@ const allDomains = [...new Set([...defaultDomains, ...envDomains])];
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    API_URL: process.env.API_URL
+    API_URL: process.env.API_URL,
+    SHOW_GIT_VERSION: process.env.SHOW_GIT_VERSION,
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    NEXT_PUBLIC_GIT_VERSION_ID: process.env.NEXT_PUBLIC_GIT_VERSION_ID
   },
   images: {
     remotePatterns: allDomains.map(domain => ({
