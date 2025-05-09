fd180cf8e0bbec8aab14992dfe2b48237b49d97e
rahulstratagile <rahul.v@stratagile.com>
2025-05-09 15:04:51 +0530

    store url query to for navigate

--- src/hooks/useAppNavigation.ts
+++ src/hooks/useAppNavigation.ts
@@ -1,30 +1,33 @@
 "use client";
 
 import { useEffect } from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { useNavigationStore } from "@/store/navigationStore";
 
 export const useAppNavigation = () => {
     const router = useRouter();
     const pathName = usePathname();
+    const searchParams = useSearchParams();
 
+    
     const { addPath, getPreviousPath } = useNavigationStore();
-
+    
+    const fullUrl = searchParams.toString() ? `${pathName}?${searchParams.toString()}` : pathName;
+    
+    console.log('[useAppNavigation] History:', useNavigationStore.getState().history);
+    
     // Record the current path on mount
     useEffect(() => {
-        console.log('[useAppNavigation] Current path:', pathName);
-        addPath(pathName);
-    }, [pathName, addPath])
+        addPath(fullUrl);
+    }, [fullUrl, addPath])
 
     const navigateToPreviousPage = () => {
         const previousPath = getPreviousPath();
         console.log('[useAppNavigation] Retrieved previous path:', previousPath);
 
         if (previousPath) {
-            console.log('[useAppNavigation] Navigating to:', previousPath);
             router.push(previousPath)
         } else {
-            console.log('[useAppNavigation] No previous path found. Navigating to root.');
             router.push("/dashboard");
         }
     };
