eb94abec5f95be1f47cfea8b58789cee34889e9c
rahulstratagile <rahul.v@stratagile.com>
2025-05-13 12:07:29 +0530

    add loader in aseet generation

--- src/app/generate-asset/page.tsx
+++ src/app/generate-asset/page.tsx
@@ -1,37 +1,28 @@
 'use client'
 
-import React, {
-  FC,
-  Suspense,
-  useMemo
-} from 'react';
-import ProgressSection from "./components/progressSection"
+import React, { Suspense, lazy } from 'react';
 import { useSearchParams } from 'next/navigation';
-import { PageType } from '@/componentsMap/pageMap';
+import type { PageType } from '@/componentsMap/pageMap';
 
+const ProgressSection = lazy(() => import('./components/progressSection'));
 
-const GenerateAssetContent: FC = () => {
-  const params = useSearchParams()
+const LoadingSpinner = () => (
+  <div className="flex justify-center items-center min-h-screen">
+    <div className="w-10 h-10 border-4 border-t-green-500 border-gray-300 rounded-full animate-spin"></div>
+  </div>
+);
 
-  const assetType = useMemo(() => {
-    return params.get('asset-type') || '';
-  }, [params]);
+export default function GenerateAssetPage() {
+  const params = useSearchParams();
+
+  const assetType = params.get('asset-type') as PageType;
 
   return (
-    <>
-      <div className="overflow-x-hidden">
-        <ProgressSection params={{ type_page: assetType as PageType }} />
-      </div>
-    </>
+    <div className="overflow-x-hidden">
+      <Suspense fallback={<LoadingSpinner />}>
+        <ProgressSection params={{ type_page: assetType }} />
+      </Suspense>
+    </div>
   )
 }
 
-const GenerateAssetPage = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <GenerateAssetContent />
-    </Suspense>
-  );
-};
-
-export default GenerateAssetPage
