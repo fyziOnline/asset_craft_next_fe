aa77a7318f108f20763e6bf14f91fa8ea5a27e6a
nikhil <nikhil.s@stratagile.com>
2025-06-03 17:22:58 +0530

    asset presentation issue on refreshing edit screen

--- Preloader.tsx
+++ Preloader.tsx
@@ -2,20 +2,30 @@ import React from "react";
 
 interface PreloaderProps {
     rowCount?: number;
+    forEnhancedShadowDom?: boolean;
 }
 
-const Preloader: React.FC<PreloaderProps> = ({ rowCount = 3 }) => {
+const Preloader: React.FC<PreloaderProps> = ({  forEnhancedShadowDom = false }) => {
     return (
         <div className="w-full">
-            <div className="grid gap-3">
-                <div className="relative skeleton-row">
-                    <div className="skeleton-wave"></div>
+            {forEnhancedShadowDom ? (
+                // Customized for EnhancedShadowDomContainer
+              <div className="relative h-screen bg-gray-300 overflow-hidden rounded-md">
+                    <div className="skeleton-wave" />
                 </div>
-                <div className="relative skeleton-row">
-                    <div className="skeleton-wave"></div>
+            ) : (
+                // Default skeleton rows
+                <div className="grid gap-3">
+                    <div className="relative skeleton-row">
+                        <div className="skeleton-wave"></div>
+                    </div>
+                    <div className="relative skeleton-row">
+                        <div className="skeleton-wave"></div>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
+
     );
 };
 
