b078e0c715768db6566d70eb16624e7d95664707
Thomas chacko <165127786+thomas-stratagile@users.noreply.github.com>
2025-06-26 16:36:17 +0530

    dynamic import added for lottie loader

--- src/components/global/Loading/LoadingOverlay.tsx
+++ src/components/global/Loading/LoadingOverlay.tsx
@@ -1,13 +1,31 @@
 import React, { memo } from 'react';
-import { Player } from '@lottiefiles/react-lottie-player';
+import dynamic from 'next/dynamic';
+
 import loadingAnimation from '../../../../public/loadingIcon.json';
 
-const LoadingOverlay: React.FC<{ loading: boolean }> = ({ loading }) => {
+// Dynamically import
+const DynamicPlayer = dynamic(
+    () => import('@lottiefiles/react-lottie-player').then(mod => ({ default: mod.Player })),
+    { 
+        ssr: false,
+        loading: () => (
+            <div className="flex justify-center items-center">
+                <div className="animate-pulse bg-gray-200 rounded-full h-[400px] w-[400px]" />
+            </div>
+        )
+    }
+);
+
+interface LoadingOverlayProps {
+    loading: boolean;
+}
+
+const LoadingOverlay: React.FC<LoadingOverlayProps> = ({ loading }) => {
     if (!loading) return null;
 
     return (
         <div className="fixed inset-0 bg-[#d0cece54] backdrop-blur-[2px] flex justify-center items-center z-[1000] pointer-events-none">
-            <Player
+            <DynamicPlayer
                 src={loadingAnimation}
                 loop
                 autoplay
